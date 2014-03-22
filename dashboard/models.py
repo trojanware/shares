c2183fb36bc7a293d8bbbfee17a27c40452a13c1
trojanware <silverspade_vaibhav@hotmail.com>
2014-03-23 03:12:22 +0530

    initial commit

---
+++ dashboard/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Scrip(models.Model):
+    scrip_id = models.CharField(max_length=100, primary_key=True)
+    exchange = models.CharField(max_length=10)
+    scrip_name = models.CharField(max_length=200)
+    mkt_value = models.FloatField()
+
+
+class Transaction(models.Model):
+    date = models.DateTimeField()
+    scrip = models.ForeignKey(Scrip)
+    transaction_type = models.CharField(max_length=4)
+    rate = models.FloatField()
+    qty = models.IntegerField()
+
