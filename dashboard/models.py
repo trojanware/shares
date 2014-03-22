7e953db6ac4576887e696df9bb5ea5a3d765bf9e
trojanware <silverspade_vaibhav@hotmail.com>
2014-03-23 03:17:49 +0530

    should be reverted

--- dashboard/models.py
+++ dashboard/models.py
@@ -5,7 +5,7 @@ class Scrip(models.Model):
     scrip_id = models.CharField(max_length=100, primary_key=True)
     exchange = models.CharField(max_length=10)
     scrip_name = models.CharField(max_length=200)
-    mkt_value = models.FloatField()
+    mkt_value = models.FloatField(
 
 
 class Transaction(models.Model):
